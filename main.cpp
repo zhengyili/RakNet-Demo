68ee6b852bac0b595f52b7b7e7b1a8ed9485e065
Dreamsxin <dreamsxin@126.com>
2014-10-17 08:52:01 +0800

    Init

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Dreamszhu
+ *
+ * Created on October 17, 2014, 8:30 AM
+ */
+
+#include <cstdlib>
+
+#include "include/raknet/MessageIdentifiers.h"
+#include "include/raknet/RakPeerInterface.h"
+#include "include/raknet/RakNetTypes.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
